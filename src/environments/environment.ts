4dad6658bf3fa116da8fec323382881a55860e85
Santiago Mejia <BSANTIAGO@oj.gob.gt>
2019-09-06 10:52:53 -0600

    configuracion fireBase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireBaseConfig:{
+    apiKey: "AIzaSyCDC22Nwis6Q9RHUSm9qzCuMLadFefhzNY",
+    authDomain: "loginappfirebase-99e19.firebaseapp.com",
+    databaseURL: "https://loginappfirebase-99e19.firebaseio.com",
+    projectId: "loginappfirebase-99e19",
+    storageBucket: "",
+    messagingSenderId: "673664096675",
+    appId: "1:673664096675:web:817b599303f34b0af02216"
+  }
+  //hace laconexión con la plataforma de fireBase
+  //una base de datos para dev and prod recomendacion
 };
 
 /*
